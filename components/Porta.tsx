2990c9b7196054a28f428d6ac116c989ec9f674f
Daniel Sampaio <danielsampaio127@gmail.com>
2024-08-08 22:01:31 -0300

    feat: Integrando useState, Typando contantes privadas e vinculando numero da porta a variavel numero.

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,13 +1,19 @@
+import PortaModel from "@/model/porta"
 import style from "../styles/Porta.module.css"
 
-export default function Porta(props: any) {
-const selecionada : string = props.selecionada ? style.selecionada : ''
+interface PortaProps {
+    porta: PortaModel
+}
+
+export default function Porta(props: PortaProps) {
+    const {porta} = props
+    const selecionada: string = porta.selecionada ? style.selecionada : ''
 
     return (
         <div className={style.area}>
             <div className={`${style.estrutura} ${selecionada}`}>
                 <div className={style.porta}>
-                    <div className={style.numero}>3</div>
+                    <div className={style.numero}>{porta.numero}</div>
                     <div className={style.macaneta}></div>
                 </div>
             </div>
